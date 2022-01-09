ad471888e891f5b25208411807d424e3fbb259b3
Vaibhav Anup Mahajan <vaibhav.mahajan@elliemae.com>
2022-01-09 20:09:10 +0530

    initial commit

---
+++ src/com/java/string/reverse/ReverseString.java
@@ -0,0 +1,19 @@
+package com.java.string.reverse;
+
+public class ReverseString
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		String str = "India";
+				
+		for (int i = str.length()-1; i >= 0 ; i--) 
+		{			
+			System.out.print(str.charAt(i));
+		}
+
+	}
+
+}
